23689af7813bc3f264f07c3e7e3d7bbbd4b682b0
PranitK <Pranit.Khadilkar@talentica.com>
2023-03-21 10:27:58 +0530

    code setup

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Post } from '@nestjs/common'
+import { Param } from '@nestjs/common/decorators'
+
+@Controller('cats')
+export class CatsController {
+  @Post()
+  create(): string {
+    return 'This action adds a new cat'
+  }
+
+  @Get()
+  findAll(): string {
+    return 'This action returns all cats'
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    return `This action returns a #${id} cat`
+  }
+}

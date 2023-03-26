c9662357d912b1bb4712701c4eb51d40498c37bd
PranitK <Pranit.Khadilkar@talentica.com>
2023-03-26 19:05:06 +0530

    added CRUD for users table

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Query,
+  Delete,
+  Patch,
+  NotFoundException,
+} from '@nestjs/common'
 import { CreateUserDto } from './dtos/create-user.dto'
+import { UpdateUserDto } from './dtos/update-user.dto'
 import { UsersService } from './users.service'
 
 @Controller('auth')
@@ -9,5 +21,32 @@ export class UsersController {
   @Post('signup')
   createUser(@Body() body: CreateUserDto) {
     this.usersService.create(body.email, body.password)
+  }
+
+  @Get(':id')
+  async findUser(@Param('id', ParseIntPipe) id: number) {
+    const user = await this.usersService.findOne(id)
+    if (!user) {
+      throw new NotFoundException('user not found')
+    }
+    return user
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email)
+  }
+
+  @Delete(':id')
+  removeUser(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.remove(id)
+  }
+
+  @Patch(':id')
+  updateUser(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() body: UpdateUserDto,
+  ) {
+    return this.usersService.update(id, body)
   }
 }

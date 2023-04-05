3e41d54b35d3a6e006d0655f551b8883a31f5f71
PranitK <Pranit.Khadilkar@talentica.com>
2023-04-05 16:42:14 +0530

    added test case for signin and whoami routes

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -15,7 +15,7 @@ describe('Authentication System', () => {
     await app.init()
   })
 
-  it('should handle the signup request', () => {
+  it('should handle the signin request', () => {
     return request(app.getHttpServer())
       .post('/auth/signup')
       .send({ email: 'foo1@gmail.com', password: 'password' })
@@ -25,5 +25,18 @@ describe('Authentication System', () => {
         expect(id).toBeDefined()
         expect(email).toEqual('foo1@gmail.com')
       })
+  })
+
+  it('should handle the whoami request for logged in user', async () => {
+    const res = await request(app.getHttpServer())
+      .post('/auth/signin')
+      .send({ email: 'foo1@gmail.com', password: 'password' })
+      .expect(201)
+    const cookie = res.get('Set-Cookie')
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200)
+    expect(body.email).toEqual('foo1@gmail.com')
   })
 })

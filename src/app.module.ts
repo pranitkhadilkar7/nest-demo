fda9c7fe6806f7d291643c730233fa7e7ef55c6f
PranitK <Pranit.Khadilkar@talentica.com>
2023-04-05 13:01:46 +0530

    added end to end test case for signup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common'
+import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { CatsController } from './cats/cats.controller'
@@ -8,6 +8,9 @@ import { ReportsModule } from './reports/reports.module'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { User } from './users/user.entity'
 import { Report } from './reports/report.entity'
+import { APP_PIPE } from '@nestjs/core'
+
+const cookieSession = require('cookie-session')
 
 @Module({
   imports: [
@@ -22,6 +25,13 @@ import { Report } from './reports/report.entity'
     ReportsModule,
   ],
   controllers: [AppController, CatsController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    { provide: APP_PIPE, useValue: new ValidationPipe({ whitelist: true }) },
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(cookieSession({ keys: ['ekcmekaqwpqlkw'] })).forRoutes('*')
+  }
+}

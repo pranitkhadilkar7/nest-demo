5d8c26c0c25ee7ce5d1ce1dd359fd4ab79a8e388
PranitK <Pranit.Khadilkar@talentica.com>
2023-04-15 16:42:42 +0530

    added price estimator function

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,10 +1,12 @@
 import {
   Body,
   Controller,
+  Get,
   Param,
   ParseIntPipe,
   Patch,
   Post,
+  Query,
   UseGuards,
 } from '@nestjs/common'
 import { CreateReportDto } from './dtos/create-report.dto'
@@ -16,10 +18,16 @@ import { Serialize } from 'src/interceptors/serialize.interceptor'
 import { ReportDto } from './dtos/report.dto'
 import { ApproveReportDto } from './dtos/approve-report.dto'
 import { AdminGuard } from 'src/guards/admin.guard'
+import { GetEstimateDto } from './dtos/get-estimate.dto'
 
 @Controller('reports')
 export class ReportsController {
   constructor(private reportsService: ReportsService) {}
+
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto) {
+    return this.reportsService.createEstimate(query)
+  }
 
   @Post()
   @UseGuards(AuthGuard)
